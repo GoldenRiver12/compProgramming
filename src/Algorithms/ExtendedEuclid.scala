c2688883a9e4ed0b05f1a654df752a4f12b1fe01
goldenriver <gandhi9736@gmail.com>
2021-04-29 22:48:13 +0900

    [add]拡張ユークリッド互除法アルゴリズムクラス追加

--- src/Algorithms/ExtendedEuclid.scala
+++ src/Algorithms/ExtendedEuclid.scala
@@ -1,5 +1,34 @@
 package Algorithms
 
+import EnrichMyLibrary.RichIntegralOps
+
 object ExtendedEuclid {
 
+  /**
+   * 拡張ユークリッド互除法
+   * @param a
+   * @param b
+   * @return  ax + by = gcd(a,b) なる整数組(x,y)
+   */
+  def gcdExt[T](a:T,b:T)(implicit int:Integral[T]):(T,T) ={
+    import int._
+    if(b == zero)
+      (one,zero)
+    else {
+      val q = a /+ b
+      val r = a %+ b
+      val (x_, y_) = gcdExt(b, r)
+      (y_, x_ - q * y_)
+    }
+  }
+
+
+
+
+
+
+
+
 }
+
+
