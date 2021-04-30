46de5b570d18861751bef635e6b7c87cf091447b
goldenriver <gandhi9736@gmail.com>
2021-04-30 16:03:25 +0900

    [add]コメント追加

--- src/enrichMyLibrary/package.scala
+++ src/enrichMyLibrary/package.scala
@@ -1,12 +1,24 @@
 package object enrichMyLibrary {
   implicit class RichIntegralOps[T](val t1:T)(implicit int:Integral[T]){
     import int._
+
+    /**
+     * 剰余が非負になる除算（商）
+     * @param t2
+     * @return
+     */
     def /+(t2:T):T ={
       if(t1 % t2 >= zero)
         t1 / t2
       else
         t1 / t2 - t2.sign
     }
+
+    /**
+     * 剰余が非負になる除算（剰余）
+     * @param t2
+     * @return
+     */
     def %+(t2:T):T ={
       val mod = t1 % t2
       if(mod >= zero)
