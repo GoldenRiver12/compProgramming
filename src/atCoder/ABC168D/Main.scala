4db50043f25dc49cf72ee49f55d322b131dc364b
goldenriver <gandhi9736@gmail.com>
2021-05-02 12:07:37 +0900

    [update]ABC168D(AC)

--- src/atCoder/ABC168D/Main.scala
+++ src/atCoder/ABC168D/Main.scala
@@ -1,87 +1,99 @@
 package atCoder.ABC168D
 
 import scala.collection.mutable
+import scala.util.Random
 
 object Main extends App{
   val sc = new java.util.Scanner(System.in)
+  val debugMode = false
+  val dl = new DebugLogger(debugMode)
+
+
+
   val n = sc.nextInt()
   val m = sc.nextInt()
-  val edges = Seq.fill(m)((sc.nextInt(),sc.nextInt()))
+
+  Timer.reset()
+
+  val edges = mutable.ArrayBuffer.fill(m+3)(mutable.ArrayBuffer.empty[Int])
+  for(_ <- 1 to m){
+    val v1,v2 = sc.nextInt()
+    edges(v1).addOne(v2)
+    edges(v2).addOne(v1)
+  }
+
+  dl.log(Timer.getExecTimeMsg())
+
+  var arrowMap = mutable.Seq.fill(n+3)(-1)
+  var arrived = mutable.Seq.fill(n+3)(false)
+
+  val graph = Graph(1 to n, edges, edges(_))
 
 
-  var arrowMap = Map.empty[Int,Int]
-  var arrived = Set.empty[Int]
-
-  val graph = Graph.of((1 to n).toSet,edges)
   val queue = mutable.Queue.empty[Int]
   queue.enqueue(1)
 
-  //var cnt=0
-  arrived += 1
+  dl.log(Timer.getExecTimeMsg())
+
+  var cnt=0
+  arrived.update(1,true)
   while(queue.nonEmpty){
     val v = queue.dequeue()
-    //cnt += graph.adjMap(v).size
-    for(adj <- graph.adjMap(v) if !arrived.contains(adj)){
+    cnt += graph.getAdj(v).size
+    for(adj <- graph.getAdj(v) if !arrived(adj)){
       queue.enqueue(adj)
-      arrived += adj
-      arrowMap += adj -> v
+      arrived.update(adj,true)
+      arrowMap.update(adj,v)
     }
   }
-  //println(s"cnt:$cnt")
+  dl.log(s"cnt:$cnt")
+
+  dl.log(Timer.getExecTimeMsg())
+
   println("Yes")
   for(v <- 2 to n){
     println(arrowMap(v))
   }
 
 
+  dl.log(Timer.getExecTimeMsg())
+
+  case class Graph[V,E](vertices:Iterable[V],edges:E, adj:V => Iterable[V]) {
+    def getAdj(v:V):Iterable[V]={
+      adj(v)
+    }
+  }
 
 
-  class Graph[V](val vertices:Set[V],val adjMap:Map[V,Set[V]]) {
-    def dfs(start:V):Seq[V] = {
-      require(vertices.contains(start))
-      var arrived = Set.empty[V]
-      val queue = collection.mutable.Queue.empty[V]
-      var ret = Seq.empty[V]
 
-      queue.enqueue(start)
-      arrived += start
-      while(queue.nonEmpty){
-        val v = queue.dequeue
-        ret :+= v
-        val next = adjMap(v).filter(!arrived.contains(_))
-        queue.enqueueAll(next)
-        arrived ++= next
-      }
-      ret
+  object Timer{
+    val main:String = new Throwable().getStackTrace()(1).getMethodName
+    var time:Long = System.currentTimeMillis()
+
+    def reset():Unit={
+      time = System.currentTimeMillis()
     }
 
-    def bfs(start:V):Seq[V]={
-      require(vertices.contains(start))
-      var arrived = Set.empty[V]
-      var ret = Seq.empty[V]
+    def getExecTimeMsg(name:String = ""):String ={
+      val lineNo = new Throwable()
+        .getStackTrace
+        .filter(_.getMethodName == main).head
+        .getLineNumber
+      s"$name 処理時間： $getExecTime ミリ秒 （行 : $lineNo）"
+    }
 
-      def bfsInner(v:V):Unit ={
-        arrived += v
-        ret :+= v
-        for(nbhd <- adjMap(v)) {
-          if (!arrived.contains(nbhd)) {
-            bfsInner(nbhd)
-          }
-        }
-      }
-      bfsInner(start)
+    def getExecTime:Long={
+      val ret = System.currentTimeMillis - time
+      reset()
       ret
     }
   }
 
-  object Graph{
-    def of[V](vertices:Set[V],edges:Seq[(V,V)]):Graph[V]={
-      val edgesRev = edges.map{case (v1,v2)=>(v2,v1)}
-      val adjMap = (edges ++ edgesRev).groupMap(_._1)(_._2)
-        .map{case(k,v)=>(k,v.toSet)}
-        .withDefaultValue(Set.empty)
-
-      new Graph[V](vertices,adjMap)
+  class DebugLogger(val debugMode:Boolean){
+    def log(s:Any):Unit={
+      if(debugMode)
+        println(s.toString)
     }
+
   }
 }
